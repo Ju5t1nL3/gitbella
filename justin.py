97b2de9811c1633a9d73207fc30c81e06d718396
Ju5t1nL3 <justinle2006@gmail.com>
2026-05-15 17:12:31 -0500

    Add 2 functions

--- justin.py
+++ justin.py
@@ -1,1 +1,17 @@
 print("hello worldddddsssss")
+
+
+
+
+
+
+##### justin's function
+def print_100():
+    for i in range(100):
+        print(i)
+
+
+#### bellas function
+def print_50():
+    for i in range(50):
+        print(i)
